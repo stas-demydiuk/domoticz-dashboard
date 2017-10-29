c18f1869d4576cb70d9013af31abdf499ac804fb
Stanislav Demydiuk <s.demydiuk@gmail.com>
2017-10-29 17:44:59 +0200

    Docker support

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const path = require('path');
+
+const PORT = process.env.PORT || 8080;
+const HOST = process.env.HOST || '0.0.0.0';
+
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
@@ -8,4 +12,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(9000, '0.0.0.0');
+app.listen(PORT, HOST, () => {
+    console.log(`Running on http://${HOST}:${PORT}`);
+});
